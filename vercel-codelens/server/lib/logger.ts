4ec2e03e405f6009e42269d8f81f7f83db9811dd
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-07-15 04:00:59 -0300

    Publicado pelo SK Code Editor

---
+++ vercel-codelens/server/lib/logger.ts
@@ -0,0 +1,8 @@
+export const logger = {
+  info: (...args: any[]) => console.log(...args),
+  error: (...args: any[]) => console.error(...args),
+  warn: (...args: any[]) => console.warn(...args),
+  debug: (...args: any[]) => console.debug(...args),
+  fatal: (...args: any[]) => console.error(...args),
+  child: () => logger,
+};
